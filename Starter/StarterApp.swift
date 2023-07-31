67640bf9e82e8ec73ea4f2e1684354e008a27e66
wbilihatu <wbilihatu@gmail.com>
2023-07-31 10:27:03 +0300

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by Woynshet Bilihatu on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
